7769f98c1a7c3c5336450544a66158f93da2f75f
Gianluca Mardente <gianluca.mardente@gmail.com>
2025-12-03 09:12:50 +0100

    (chore) advance to golang v1.25.5

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -184,7 +184,7 @@ func getClassifierReport(classifierName, clusterNamespace, clusterName string) *
 }
 
 func getClassifierInstance(name string) *libsveltosv1beta1.Classifier {
-	classifierLabels := []libsveltosv1beta1.ClassifierLabel{{Key: "version", Value: "v1.25.4"}}
+	classifierLabels := []libsveltosv1beta1.ClassifierLabel{{Key: "version", Value: "v1.25.5"}}
 	return &libsveltosv1beta1.Classifier{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -192,7 +192,7 @@ func getClassifierInstance(name string) *libsveltosv1beta1.Classifier {
 		Spec: libsveltosv1beta1.ClassifierSpec{
 			KubernetesVersionConstraints: []libsveltosv1beta1.KubernetesVersionConstraint{
 				{
-					Version:    "1.25.4",
+					Version:    "1.25.5",
 					Comparison: string(libsveltosv1beta1.ComparisonEqual),
 				},
 			},
