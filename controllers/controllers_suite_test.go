6a8d78c5ba9abd04f851aedc37ccdc514b78da8a
Gianluca Mardente <gianluca.mardente@gmail.com>
2026-05-15 16:23:15 +0200

    (chore) prepare for release v1.10.0

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -185,7 +185,7 @@ func getClassifierReport(classifierName, clusterNamespace, clusterName string) *
 }
 
 func getClassifierInstance(name string) *libsveltosv1beta1.Classifier {
-	classifierLabels := []libsveltosv1beta1.ClassifierLabel{{Key: "version", Value: "v1.26.2"}}
+	classifierLabels := []libsveltosv1beta1.ClassifierLabel{{Key: "version", Value: "v1.26.3"}}
 	return &libsveltosv1beta1.Classifier{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -193,7 +193,7 @@ func getClassifierInstance(name string) *libsveltosv1beta1.Classifier {
 		Spec: libsveltosv1beta1.ClassifierSpec{
 			KubernetesVersionConstraints: []libsveltosv1beta1.KubernetesVersionConstraint{
 				{
-					Version:    "1.26.2",
+					Version:    "1.26.3",
 					Comparison: string(libsveltosv1beta1.ComparisonEqual),
 				},
 			},
