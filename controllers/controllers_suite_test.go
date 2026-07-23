5a89d7bd387ee2ce3469585fc4948086e2ac7c62
Gianluca Mardente <gianluca.mardente@gmail.com>
2026-07-23 14:08:47 +0200

    chore: advance dependencies

--- controllers/controllers_suite_test.go
+++ controllers/controllers_suite_test.go
@@ -197,7 +197,7 @@ func getClassifierReport(classifierName, clusterNamespace, clusterName string) *
 }
 
 func getClassifierInstance(name string) *libsveltosv1beta1.Classifier {
-	classifierLabels := []libsveltosv1beta1.ClassifierLabel{{Key: classifierLabelVersion, Value: "v1.26.4"}}
+	classifierLabels := []libsveltosv1beta1.ClassifierLabel{{Key: classifierLabelVersion, Value: "v1.26.5"}}
 	return &libsveltosv1beta1.Classifier{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: name,
@@ -205,7 +205,7 @@ func getClassifierInstance(name string) *libsveltosv1beta1.Classifier {
 		Spec: libsveltosv1beta1.ClassifierSpec{
 			KubernetesVersionConstraints: []libsveltosv1beta1.KubernetesVersionConstraint{
 				{
-					Version:    "1.26.4",
+					Version:    "1.26.5",
 					Comparison: string(libsveltosv1beta1.ComparisonEqual),
 				},
 			},
